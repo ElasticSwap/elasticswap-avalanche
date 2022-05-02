b227ac9f35e6b4c13f87295083b2a2d11afde18b
Harman Kamboj <playcivil@gmail.com>
2022-05-02 20:10:41 +0530

    Add Token Definitions to Exchanges

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,35 +1,72 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 import {
   ExchangeFactory,
   NewExchange,
   OwnershipTransferred,
   SetFeeAddress,
 } from "../generated/ExchangeFactory/ExchangeFactory";
-import { ExampleEntity } from "../generated/schema";
+import { Exchange as ExchangeContract } from "../generated/ExchangeFactory/Exchange";
+import { ERC20 } from "../generated/ExchangeFactory/ERC20";
+import { Exchange, Token } from "../generated/schema";
 
 export function handleNewExchange(event: NewExchange): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.hash.toHex());
+  // let entity = ExampleEntity.load(event.transaction.from.toHex())
 
+  let exchange = Exchange.load(event.transaction.hash.toHex());
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.hash.toHex());
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0);
+  if (!exchange) {
+    // entity = new ExampleEntity(event.transaction.from.toHex())
+    exchange = new Exchange(event.transaction.hash.toHex());
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1);
-
   // Entity fields can be set based on event parameters
-  entity.creator = event.params.creator;
-  entity.exchangeAddress = event.params.exchangeAddress;
+  exchange.creator = event.params.creator;
+  exchange.exchangeAddress = event.params.exchangeAddress;
+
+  let exchangeContract = ExchangeContract.bind(event.params.exchangeAddress);
+
+  let baseToken = Token.load(exchangeContract.baseToken().toHex());
+  if (!baseToken) {
+    baseToken = new Token(exchangeContract.baseToken().toHex());
+  }
+  baseToken.symbol = ERC20.bind(exchangeContract.baseToken()).symbol();
+  baseToken.name = ERC20.bind(exchangeContract.baseToken()).name();
+  baseToken.totalSupply = ERC20.bind(
+    exchangeContract.baseToken()
+  ).totalSupply();
+  baseToken.decimals = BigDecimal.fromString(
+    ERC20.bind(exchangeContract.baseToken())
+      .decimals()
+      .toString()
+  );
+  baseToken.exchangeFactoryAddress = exchangeContract.exchangeFactoryAddress();
+  baseToken.save();
+
+  let quoteToken = Token.load(exchangeContract.quoteToken().toHex());
+  if (!quoteToken) {
+    quoteToken = new Token(exchangeContract.quoteToken().toHex());
+  }
+  quoteToken.symbol = ERC20.bind(exchangeContract.quoteToken()).symbol();
+  quoteToken.name = ERC20.bind(exchangeContract.quoteToken()).name();
+  quoteToken.totalSupply = ERC20.bind(
+    exchangeContract.quoteToken()
+  ).totalSupply();
+  quoteToken.decimals = BigDecimal.fromString(
+    ERC20.bind(exchangeContract.quoteToken())
+      .decimals()
+      .toString()
+  );
+  quoteToken.exchangeFactoryAddress = exchangeContract.exchangeFactoryAddress();
+  quoteToken.save();
+
+  exchange.baseToken = baseToken.id;
+  exchange.quoteToken = quoteToken.id;
 
   // Entities can be written to the store with `.save()`
-  entity.save();
+  exchange.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -52,6 +89,6 @@ export function handleNewExchange(event: NewExchange): void {
   // - contract.owner(...)
 }
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+// export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handleSetFeeAddress(event: SetFeeAddress): void {}
+// export function handleSetFeeAddress(event: SetFeeAddress): void {}
